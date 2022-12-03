be2f9a62ad9e9edd4c817f91f5e0b670ed9f1306
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2022-12-03 15:33:04 +0100

    Delete function is ok

--- index.php
+++ index.php
@@ -1,42 +1,80 @@
-<?php  
-    if(isset($_POST["contenu"])){
-        $fichier = "./files/roswell/". $_POST['file'];
-        $file= fopen($fichier, "w");
-        fwrite($file, stripslashes($_POST["contenu"]));
-        fclose($file);
-    }
+<?php
+
+if (isset($_POST["contenu"])) {
+    $fichier = "./files/roswell/" . $_POST['file'];
+    $file = fopen($fichier, "w");
+    fwrite($file, stripslashes($_POST["contenu"]));
+    fclose($file);
+}
 ?>
-<?php include('inc/head.php'); ?>
+<?php include 'inc/head.php';?>
+
 
 <?php
-$result = array();
-$check = array();
-foreach($array as $val) {
-     $str = pathToArrayStr($val, 'result');
-     foreach($check as $ck) {
-         if (strpos($ck, $str) !== false) {
-             continue 2;
-         }
-     }
-     $check[] = $str;
-     eval('$result'.$str.' = array();');
+/*---------------------------------------------------------------*/
+/*
+Titre : Efface un répertoire et ses sous répertoires
+
+URL   : https://phpsources.net/code_s.php?id=513
+Auteur           : evanxg852000
+Date édition     : 11 Mai 2009
+Date mise à jour : 19 Aout 2019
+Rapport de la maj:
+- fonctionnement du code vérifié
+ */
+/*---------------------------------------------------------------*/
+
+function RepEfface($dir)
+{
+    $handle = opendir($dir);
+    while ($elem = readdir($handle)) {
+        if (is_dir($dir . '/' . $elem) && substr($elem, -2, 2) !== '..' && substr(
+            $elem, -1, 1) !== '.') {
+            RepEfface($dir . '/' . $elem);
+        } else {
+            if (substr($elem, -2, 2) !== '..' && substr($elem, -1, 1) !== '.') {
+                unlink($dir . '/' . $elem);
+            }
+        }
+
+    }
+
+    $handle = opendir($dir);
+    while ($elem = readdir($handle)) {
+        if (is_dir($dir . '/' . $elem) && substr($elem, -2, 2) !== '..' && substr(
+            $elem, -1, 1) !== '.') {
+            RepEfface($dir . '/' . $elem);
+            rmdir($dir . '/' . $elem);
+        }
+
+    }
+    rmdir($dir);
 }
-print_r($result);
 ?>
 
-<?php 
-    $dir = opendir("./files/roswell");
-    while($file = readdir($dir)){
-        if(str_contains($file, ".txt") || str_contains($file, ".html")){
-            echo '<a href="?f='.$file.'">';
-            echo $file;
-            echo '</a>';
+
+
+<?php
+function ScanDirectory($Directory)
+{
+    $MyDirectory = opendir($Directory) or die('Erreur');
+    while ($Entry = @readdir($MyDirectory)) {
+        if (is_dir($Directory . '/' . $Entry) && $Entry != '.' && $Entry != '..') {
+            ScanDirectory($Directory . '/' . $Entry);
+        } elseif ($Entry != '.' && $Entry != '..' && (str_contains($Entry, ".txt") || str_contains($Entry, ".html"))) {
+            echo '</li>';
+            echo '<p>' . $Directory . '<button type="submit" href="RepEfface($Directory)">DELETE REPO</button><a href="?f=' . $Entry . '">' . $Entry . '</a>';
+            echo '</li>';
         }
     }
- ?>
- 
+    closedir($MyDirectory);
+}
+
+$dir = ScanDirectory("./files/");
+?>
+
 <?php
-if (isset($_GET["f"])){
+if (isset($_GET["f"])) {
     $fichier = "./files/roswell/" . $_GET["f"];
     $contenu = file_get_contents($fichier, "r") or die("Unable to open file!");
 }
@@ -50,4 +88,4 @@ if (isset($_GET["f"])){
     <input type="submit" value="Envoyer"/>
 </form>
 
-<?php include('inc/foot.php'); ?>
+<?php include 'inc/foot.php';?>
